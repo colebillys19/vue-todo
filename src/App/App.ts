835fa4dc5856d0308035227be68cc439b2a03d03
Billy Cole <billycole@macbook-pro.mynetworksettings.com>
2022-04-05 23:39:33 -0400

    Implement edit todo functionality

--- src/App/App.ts
+++ src/App/App.ts
@@ -1,10 +1,11 @@
 import { defineComponent } from "vue";
 import TodoList from "@/classes/TodoList";
+import EditDialog from "@/components/EditDialog/EditDialog.vue";
 import TodoForm from "@/components/TodoForm/TodoForm.vue";
 import TodoTable from "@/components/TodoTable/TodoTable.vue";
 
 export default defineComponent({
-  components: { TodoForm, TodoTable },
+  components: { EditDialog, TodoForm, TodoTable },
   data() {
     return {
       addInputValue: "" as string,
@@ -15,8 +16,10 @@ export default defineComponent({
   },
   methods: {
     addTodo() {
-      this.todoList.addTodo(this.addInputValue);
-      this.addInputValue = "";
+      if (this.addInputValue) {
+        this.todoList.addTodo(this.addInputValue);
+        this.addInputValue = "";
+      }
     },
     deleteTodo(id: string) {
       this.todoList.deleteTodo(id);
@@ -27,10 +30,12 @@ export default defineComponent({
     toggleStatus(id: string) {
       this.todoList.toggleStatus(id);
     },
-    updateTask(id: string) {
-      this.todoList.updateTask(this.editInputValue, id);
-      this.editId = "";
-      this.editInputValue = "";
+    updateTask() {
+      if (this.editId) {
+        this.todoList.updateTask(this.editInputValue, this.editId);
+        this.editId = "";
+        this.editInputValue = "";
+      }
     },
   },
   name: "App",
--- src/components/EditDialog/EditDialog.ts
+++ src/components/EditDialog/EditDialog.ts
@@ -1,6 +1,20 @@
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from "vue";
+import TodoForm from "@/components/TodoForm/TodoForm.vue";
 
 export default defineComponent({
-  name: 'EditDialog',
-  components: {}
+  name: "EditDialog",
+  components: { TodoForm },
+  props: {
+    editId: String,
+    modelValue: String,
+    setEditId: Function,
+    updateTask: Function,
+  },
+  setup(props, { emit }) {
+    const inputValue = computed({
+      get: () => props.modelValue,
+      set: (value) => emit("update:modelValue", value),
+    });
+    return { inputValue };
+  },
 });
