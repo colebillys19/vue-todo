85143ce8354be27cd6cc67133177d58a0e6006b5
Billy Cole <billycole@macbook-pro.mynetworksettings.com>
2022-04-05 22:16:25 -0400

    Connect form to state.

--- src/components/TodoForm/TodoForm.ts
+++ src/components/TodoForm/TodoForm.ts
@@ -1,6 +1,20 @@
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'TodoForm',
-  components: {}
+  name: "TodoForm",
+  props: {
+    handleSubmit: Function,
+    iconAlt: String,
+    iconSrc: String,
+    isEditing: Boolean,
+    labelText: String,
+    modelValue: { default: () => "", type: String },
+  },
+  setup(props, { emit }) {
+    const inputValue = computed({
+      get: () => props.modelValue,
+      set: (value) => emit("update:modelValue", value),
+    });
+    return { inputValue };
+  },
 });
